6ee11da1f5968fc7ed87385aee8a2fcfb00769f6
Vuong Quyen Mai <59490873+RevoloerO@users.noreply.github.com>
2024-12-24 16:38:52 -0500

    v0.4.11
    - Add block validation function which validate previousHash and storedHash to prevent tampering
    - Add vaidateTest.js to test function

--- NodeJS-Alchemy/Hashing/blockchain/Blockchain.js
+++ NodeJS-Alchemy/Hashing/blockchain/Blockchain.js
@@ -11,6 +11,17 @@ class Blockchain {
         nextBlock.previousHash = this.chain[this.chain.length - 1].toHash();
         this.chain.push(nextBlock);
     }
+    // Validate previous hash and stored hash
+    isValid(){
+        const height = this.chain.length-1
+        for(var i = height; i > 0; i-- ){ 
+            const realHash = this.chain[i-1].toHash().toString();
+            const storedHash = this.chain[i].previousHash.toString();
+            if(realHash !== storedHash)
+            {return false;}
+        }
+        return true;
+    }
 }
 
 module.exports = Blockchain;
