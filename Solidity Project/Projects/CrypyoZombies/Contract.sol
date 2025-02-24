6cac987c822da9eb900658552c7c5fadef09abc4
Vuong Quyen Mai <59490873+RevoloerO@users.noreply.github.com>
2025-02-24 16:36:12 -0500

    v0.5.1c

--- Solidity Project/Projects/CrypyoZombies/Contract.sol
+++ Solidity Project/Projects/CrypyoZombies/Contract.sol
@@ -2,6 +2,8 @@
 pragma solidity >=0.8.0;
 
 contract ZombieFactory {
+    // Add event to notify when a new zombie is created
+     event NewZombie(uint zombieId, string name, uint dna);
     // Add variable
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
@@ -19,6 +21,9 @@ contract ZombieFactory {
     // Add a function to create a new zombie
     function _createZombie(string memory _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
+        uint id = zombies.length - 1;
+        // Emit the event
+        emit NewZombie(id, _name, _dna);
     }
     // Add a function to generate a random DNA using the keccak256 hash function
     // Then use the modulus operator to ensure the DNA is within the range of 0 to 10^16
